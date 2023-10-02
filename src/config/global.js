d2044d16b5f5e2b660711ccc9186e791b381ab5f
veimarcm <vcelis@sena.edu.co>
2023-10-02 08:24:59 -0500

    Actualizacion Empaquetado
    Se actualiza el cfa, material, global

--- src/config/global.js
+++ src/config/global.js
@@ -191,6 +191,11 @@ export default {
       },
       {
         icono: 'fas fa-download',
+        titulo: 'Descargar pdf',
+        download: 'downloads/CFA_19_722200.zip',
+      },
+      {
+        icono: 'fas fa-download',
         titulo: 'Descargar material',
         download: 'downloads/material.zip',
       },
